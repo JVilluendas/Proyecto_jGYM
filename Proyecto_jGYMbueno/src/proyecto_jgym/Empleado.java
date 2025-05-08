8d8bf75101110385c2f073bcd55c777eed86894f
David-Pasamar-Arroyo <davidpasamararroyo@gmail.com>
2025-05-08 10:35:39 +0200

    proyecto actualizado lol

---
+++ Proyecto_jGYMbueno/src/proyecto_jgym/Empleado.java
@@ -0,0 +1,13 @@
+package proyecto_jgym;
+
+public class Empleado extends Persona {
+    private int idEmpleado;
+
+    public int getIdEmpleado() {
+        return idEmpleado;
+    }
+
+    public void setIdEmpleado(int idEmpleado) {
+        this.idEmpleado = idEmpleado;
+    }
+}
